0e7053d068bd6b0ed60d01b5915726f6579cac70
Pappu Kumar <davetweetlive@gmail.com>
2019-05-12 15:16:32 +0530

    Added classbased login logout and basic url navigations at home and other pages

--- Karya_yojana/urls.py
+++ Karya_yojana/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView
+# from .views import SecuredLoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- karya/views.py
+++ karya/views.py
@@ -3,7 +3,9 @@ from django.views import View
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 
+
 class IndexView(View):
+
     @method_decorator(login_required)
     def get(self, request, *args, **kwargs):
         return render(request, 'base.html', {})
