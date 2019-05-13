bbd4b2bc6a64ea4fab2f7c0308ab9501825ecc1f
Pappu Kumar <davetweetlive@gmail.com>
2019-05-13 23:09:08 +0530

    Design Database Completed

--- karya/models.py
+++ karya/models.py
@@ -3,22 +3,26 @@ from django.contrib.auth.models import User
 
 
 class Employee(models.Model):
-    employee_id     = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
-
+    employee_id = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
+    profile_img = models.ImageField(default = 'default.png', upload_to = 'profile_pics', blank = True, null = True)
     def __str__(self):
         return self.employee_id
 
 
 class Team(models.Model):
-    team_name       = models.CharField(max_length=255)
-    no_of_members   = models.IntegerField(default=0)
-    members         = models.ForeignKey(User, on_delete=models.CASCADE)
+    team_name   = models.CharField(max_length=255, primary_key=True)
+    member      = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.team_name
 
+
 class Project(models.Model):
-    pass
+    name    = models.CharField(max_length=255)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_by_user')
+    assigned_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='assigned_to_team')
+    create_time= models.DateTimeField()
+    team_id = models.ForeignKey(Team, on_delete=models.CASCADE)
 
 TASK_STATUS_CHOICE = (
         ('not started','Not Started'),
@@ -27,13 +31,14 @@ TASK_STATUS_CHOICE = (
         ('completed', 'Completed'),
         ('user accepted', 'User Accepted'),
 )
+
 class Task(models.Model):
     task_title      = models.CharField(max_length=255)
-    created_by      = models.OneToOneField(User, on_delete=models.CASCADE, related_name='sample1')
-    assigned_to     = models.OneToOneField(User, on_delete=models.CASCADE, related_name='sample2')
+    created_by      = models.OneToOneField(User, on_delete=models.CASCADE, related_name='created_by')
+    assigned_to     = models.OneToOneField(User, on_delete=models.CASCADE, related_name='assigned_to_user')
     create_time     = models.DateTimeField()
     user_story      = models.TextField()
-    status          = models.CharField(max_length=50, choices=TASK_STATUS_CHOICE, default='Select')
+    status          = models.CharField(max_length=50, choices=TASK_STATUS_CHOICE, default='select')
 
     def __str__(self):
         return self.task_title
