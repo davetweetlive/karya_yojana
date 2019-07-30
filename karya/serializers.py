ca339ad2bac2c0112f97931cc9d13634121b16cc
Pappu Kumar <davetweetlive@gmail.com>
2019-07-30 23:01:07 +0530

    Added Serializers

---
+++ karya/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+from .models import(
+    EmployeeProfile,
+    Project,
+    Task,
+    SubTasks,
+    Comment
+)
+
+class ProjectSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Project
+        fields = ('project_name', 'created_by', 'assigned_team', 'create_time')
+        fields = __all__
